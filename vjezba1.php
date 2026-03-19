2899a55e31d2457eb41dd6a42f96c71b4b0db628
Fox <zacnoa2.4@gmail.com>
2026-03-19 18:46:35 +0100

    vjezba1

---
+++ vjezba1.php
@@ -0,0 +1,10 @@
+<?php
+$naslov= "Prva vjezba";
+$autor ="Noa Kolombo";
+echo "<h1>$naslov</h1>";
+echo "<p>Ovu stranicu je izradio<strong>$autor</strong></p>";
+echo "<a target='_blank' href='https://www.youtube.com'>Link na Youtube</a>";
+?>
+
+<!-- Naziv datoteke je vjezba1.php -->
+
